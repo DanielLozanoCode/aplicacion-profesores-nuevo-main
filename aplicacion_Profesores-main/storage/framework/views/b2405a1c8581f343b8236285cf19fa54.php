d1eb20a56270ae221bb6f2c3a88890e39e5d0e55
danidigitech <lozano.sanchez.daniel00@gmail.com>
2024-11-29 11:45:53 +0100

    Initial commit

---
+++ aplicacion_Profesores-main/storage/framework/views/b2405a1c8581f343b8236285cf19fa54.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Iniciar Sesión</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+</head>
+<body class="flex items-center justify-center h-screen bg-gray-100">
+    <div class="w-full max-w-sm bg-white rounded-lg shadow-md p-6">
+        <h2 class="text-2xl font-bold mb-6 text-center">Iniciar Sesión</h2>
+        <form method="POST" action="<?php echo e(route('dashboard')); ?>">
+            <?php echo csrf_field(); ?>
+            <div class="mb-4">
+                <label for="email" class="block text-gray-700">Correo Electrónico</label>
+                <input type="email" id="email" name="email" class="w-full p-2 border border-gray-300 rounded" required>
+            </div>
+            <div class="mb-4">
+                <label for="password" class="block text-gray-700">Contraseña</label>
+                <input type="password" id="password" name="password" class="w-full p-2 border border-gray-300 rounded" required>
+            </div>
+            <button type="submit" class="w-full bg-blue-500 text-white py-2 rounded">Iniciar Sesión</button>
+        </form>
+        <form method="POST" action="<?php echo e(route('dashboard')); ?>">
+            <?php echo csrf_field(); ?>
+            <a href="<?php echo e(route('home')); ?>" class="w-full bg-green-500 text-white py-2 rounded mt-2">¿No tienes cuenta?¡Registrate!</a>
+        </form>
+    </div>
+</body>
+</html>
+<?php /**PATH C:\Users\fer\Documents\GitHub\aplicacion-profesores-nuevo\aplicacion_Profesores-main\resources\views/auth/login.blade.php ENDPATH**/ ?>
